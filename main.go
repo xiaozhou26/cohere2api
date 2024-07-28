2ff450b194b4c9662a2a050461a8bf8675f6dfd3
xiaozhou26 <xiaoxiaofeihh@foxmail.com>
2024-07-28 23:46:47 +0800

    init

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"cohere/initialize"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv" // 导入godotenv包
+)
+
+var (
+	Port string
+	Bind string
 )
 
 func main() {
+	// 从当前目录的.env文件加载环境变量
+	godotenv.Load()
 	// 初始化路由
 	router := initialize.InitRouter()
-
 	// 启动服务器
-	port := 8080
-	router.Run(fmt.Sprintf(":%d", port))
+	Port = os.Getenv("PORT")
+	if Port == "" {
+		Port = "8080"
+	}
+	if Bind == "" {
+		Bind = "0.0.0.0"
+	}
+	router.Run(fmt.Sprint(Bind, ":", Port))
 }
