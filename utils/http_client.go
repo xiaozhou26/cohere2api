d045633782d2f9f759907af49482938082ce0fd2
xiaozhou26 <xiaoxiaofeihh@foxmail.com>
2024-07-28 14:10:51 +0800

    init

--- utils/http_client.go
+++ utils/http_client.go
@@ -4,8 +4,21 @@ import (
 	"bytes"
 	"cohere/model"
 	"encoding/json"
+	"fmt"
+	"math/rand"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv" // 导入godotenv包
 )
+
+func init() {
+	// 从当前目录的.env文件加载环境变量
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("No .env file found")
+	}
+}
 
 func FetchChatResponse(data model.ChatData, authHeader string) (*http.Response, error) {
 	url := "https://api.cohere.ai/v1/chat"
@@ -22,6 +35,19 @@ func FetchChatResponse(data model.ChatData, authHeader string) (*http.Response, 
 	req.Header.Set("Content-Type", "application/json")
 	if authHeader != "" {
 		req.Header.Set("Authorization", authHeader)
+	} else {
+		// 从环境变量获取Bearer Token
+		tokenEnvVar := os.Getenv("COHERE_BEARER_TOKENS")
+		if tokenEnvVar == "" {
+			return nil, fmt.Errorf("environment does not contain any authorization tokens")
+		}
+		tokens := strings.Split(tokenEnvVar, ",")
+		if len(tokens) == 0 {
+			return nil, fmt.Errorf("no valid tokens found in environment variable")
+		}
+
+		selectedToken := tokens[rand.Intn(len(tokens))]
+		req.Header.Set("Authorization", selectedToken)
 	}
 	client := &http.Client{}
 	return client.Do(req)
